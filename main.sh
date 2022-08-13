5dbfc59229750cae12dc414ef480d1d2342a7693
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-13 20:27:44 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@ cd uptime-kuma
 npm run setup
 
 # Option 1. Try it
-node server/server.js
+# node server/server.js
 
 # (Recommended)
 # Option 2. Run in background using PM2
-# Install PM2 if you don't have: npm install pm2 -g
+npm install pm2 -g
 pm2 start server/server.js --name uptime-kuma
 
 #请在成功部署后注释掉第一行的 git clone
